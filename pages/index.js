115c803cecbfd2f7e32b0a7e7da15306a6b7515c
Raul.Laredo <Raul.Laredo@niceincontact.com>
2021-11-08 10:42:21 -0400

    Add initial set up with Next.js

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+const IndexPage = () => {
+  const {id,bio, first, last, favorite, twitterHandle, company, sessions} = data[0];
+  return <div>Hello From Pluralsight!</div>;
+};
+
+export default IndexPage;
